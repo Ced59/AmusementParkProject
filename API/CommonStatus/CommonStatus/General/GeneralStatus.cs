ec9ffa3c5c7981ebf9fd00bd26768b5c1b22013d
ccaud <c.caudron59@gmail.com>
2023-11-06 22:27:15 +0100

    Ajout structure interne API et ModelBase avec GeneralStatus

---
+++ API/CommonStatus/CommonStatus/General/GeneralStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace CommonStatus.General
+{
+    public enum GeneralStatus
+    {
+        OPEN,
+        DEFINITIVELY_CLOSED
+    }
+}
---
+++ API/Entities/Entities/Model/ModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonStatus.General;
+
+namespace Entities.Model
+{
+    public abstract class ModelBase
+    {
+        public Guid Id { get; set; }
+        public GeneralStatus OpenStatus { get; set; }
+    }
+}
