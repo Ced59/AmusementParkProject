aaa427b80fca157b9ae650872bde0bae07c3800f
ccaud <c.caudron59@gmail.com>
2023-11-12 22:29:43 +0100

    Amelioration barre de menu. Ajout logo et titre site. Drapeaux selection langue à améliorer.

--- FRONT/AmusementPark/src/app/common/countries-flags/countries-flags.component.ts
+++ FRONT/AmusementPark/src/app/common/countries-flags/countries-flags.component.ts
@@ -1,15 +1,43 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-countries-flags',
   standalone: true,
-  template: `<img [src]="getFlagUrl()" [alt]="countryCode" style="width: 50px; height: auto;">`
+  imports: [CommonModule],
+  template: `
+    <div (click)="toggleDropdown()">
+      <img [src]="getFlagUrl()" [alt]="countryCode" style="width: 50px; height: auto;">
+      <!-- Ajouter une icône de flèche ici si nécessaire -->
+    </div>
+    <div *ngIf="showDropdown">
+      <!-- Liste des drapeaux pour la sélection -->
+      <div *ngFor="let country of countries" (click)="selectCountry(country)">
+        <img [src]="getFlagUrl(country)" [alt]="country" style="width: 50px; height: auto;">
+      </div>
+    </div>
+  `
 })
 export class CountriesFlagsComponent {
-  @Input() countryCode: string = "US";
+  @Input() countryCode: string = "GB";
+  @Output() countrySelected = new EventEmitter<string>();
 
-  getFlagUrl(): string {
-    return `./node_modules/svg-country-flags/svg/${this.countryCode.toLowerCase()}.svg`;
+  showDropdown = false;
+  countries = ['GB', 'FR']; // Liste des codes des pays disponibles
+
+  getFlagUrl(country: string = this.countryCode): string {
+    return `./assets/svg-country-flags/svg/${country.toLowerCase()}.svg`;
+  }
+
+  toggleDropdown() {
+    this.showDropdown = !this.showDropdown;
+  }
+
+  selectCountry(country: string) {
+    this.countryCode = country;
+    this.showDropdown = false;
+    this.countrySelected.emit(country);
+    console.log("Country selected: ", country);
   }
 }
 
--- FRONT/AmusementPark/src/app/menu/menu-bar/menu-bar.component.ts
+++ FRONT/AmusementPark/src/app/menu/menu-bar/menu-bar.component.ts
@@ -2,10 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu-bar',
-  standalone: false,
   templateUrl: './menu-bar.component.html',
-  styleUrl: './menu-bar.component.css'
+  styleUrls: ['./menu-bar.component.css']
 })
 export class MenuBarComponent {
+  selectedCountry = "US";
 
+  onCountryChange(newCountry: string) {
+    this.selectedCountry = newCountry;
+    // Ici, vous pouvez ajouter d'autres logiques nécessaires lorsque le pays change
+  }
 }
+
