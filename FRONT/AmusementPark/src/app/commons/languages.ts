5614d91594dd3366b8dff01de8e2c89ec6e44eb0
Ced59 <c.caudron59@gmail.com>
2025-06-01 20:23:29 +0200

    Add some supported languages

--- FRONT/AmusementPark/src/app/commons/languages.ts
+++ FRONT/AmusementPark/src/app/commons/languages.ts
@@ -1,6 +1,10 @@
 export const LANGUAGES = [
-  { label: 'English', value: 'en', code: 'en-US' },
-  { label: 'Français', value: 'fr', code: 'fr-FR' },
-  { label: 'Español', value: 'es', code: 'es-ES' },
-  { label: 'Deutsch', value: 'de', code: 'de-DE' }
+  { label: 'English',   value: 'en', code: 'en-US' },
+  { label: 'Français',  value: 'fr', code: 'fr-FR' },
+  { label: 'Español',   value: 'es', code: 'es-ES' },
+  { label: 'Deutsch',   value: 'de', code: 'de-DE' },
+  { label: 'Italiano',  value: 'it', code: 'it-IT' },
+  { label: 'Polski',    value: 'pl', code: 'pl-PL' },
+  { label: 'Nederlands', value: 'nl', code: 'nl-NL' },
+  { label: 'Português', value: 'pt', code: 'pt-PT' }
 ];
