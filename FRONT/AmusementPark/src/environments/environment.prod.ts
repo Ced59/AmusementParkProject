43d30680a4d5c2ed8de6a456c91ddd52fb1c4794
Ced59 <c.caudron59@gmail.com>
2026-04-02 15:35:09 +0200

    Correction build angular 19

--- FRONT/AmusementPark/src/environments/environment.prod.ts
+++ FRONT/AmusementPark/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  apiBaseUrl: 'https://yourproductionapi.com/',
   baseUrl: 'https://yourproductionapi.com/',
   apiImagePath: 'https://yourproductionapi.com/images/',
   googleClientId: '819235253823-lj2de5lllhlngr4rpt5iq0m5mgmmltaj.apps.googleusercontent.com',
