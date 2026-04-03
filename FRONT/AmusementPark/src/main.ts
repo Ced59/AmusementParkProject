cb7207ab4a4a4db9682c06015a3e27968b2a7940
Ced59 <c.caudron59@gmail.com>
2026-04-03 18:59:03 +0200

    Correction main.ts

--- FRONT/AmusementPark/src/main.ts
+++ FRONT/AmusementPark/src/main.ts
@@ -6,10 +6,10 @@ import { LanguageInterceptor } from './app/interceptors/language.interceptor';
 import { AuthInterceptor } from './app/interceptors/auth.interceptor';
 import { MessageService } from 'primeng/api';
 import { providePrimeNG } from 'primeng/config';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app/app-routing.module';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SelectModule } from 'primeng/select';
 import { ToolbarModule } from 'primeng/toolbar';
 import { ButtonModule } from 'primeng/button';
@@ -25,40 +25,54 @@ import { MultiSelectModule } from 'primeng/multiselect';
 import { AppComponent } from './app/app.component';
 import AmusementParkPreset from './app/config/primeng-preset';
 
-
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule, TranslateModule.forRoot({
-            loader: {
-                provide: TranslateLoader,
-                useFactory: HttpLoaderFactory,
-                deps: [HttpClient]
-            }
-        }), BrowserAnimationsModule, SelectModule, ToolbarModule, ButtonModule, FormsModule, DialogModule, InputTextModule, CardModule, TooltipModule, ToastModule, AvatarModule, PaginatorModule, MultiSelectModule),
-        provideHttpClient(withFetch(), withInterceptorsFromDi()),
-        provideAppInitializer(() => {
-            const initializerFn = initializeApp(inject(TranslationService));
-            return initializerFn();
-        }),
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: LanguageInterceptor,
-            multi: true
-        },
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: AuthInterceptor,
-            multi: true
-        },
-        MessageService,
-        providePrimeNG({
-            theme: {
-                preset: AmusementParkPreset,
-                options: {
-                    darkModeSelector: '.dark-mode'
-                }
-            }
-        })
-    ]
-})
-  .catch(err => console.error(err));
+  providers: [
+    importProvidersFrom(
+      AppRoutingModule,
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
+      }),
+      SelectModule,
+      ToolbarModule,
+      ButtonModule,
+      FormsModule,
+      DialogModule,
+      InputTextModule,
+      CardModule,
+      TooltipModule,
+      ToastModule,
+      AvatarModule,
+      PaginatorModule,
+      MultiSelectModule
+    ),
+    provideAnimations(),
+    provideHttpClient(withFetch(), withInterceptorsFromDi()),
+    provideAppInitializer(() => {
+      const initializerFn = initializeApp(inject(TranslationService));
+      return initializerFn();
+    }),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LanguageInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+    MessageService,
+    providePrimeNG({
+      theme: {
+        preset: AmusementParkPreset,
+        options: {
+          darkModeSelector: '.dark-mode'
+        }
+      }
+    })
+  ]
+}).catch((err) => console.error(err));
