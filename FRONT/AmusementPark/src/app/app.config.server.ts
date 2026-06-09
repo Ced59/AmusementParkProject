99b6ad8533f6262dfdc79d1a22e5feb29f4bac4e
Ced59 <c.caudron59@gmail.com>
2026-06-09 08:44:22 +0200

    Correction SSR i18n

--- FRONT/AmusementPark/src/app/app.config.server.ts
+++ FRONT/AmusementPark/src/app/app.config.server.ts
@@ -1,14 +1,20 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TranslateLoader } from '@ngx-translate/core';
 import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
 import { provideServerRendering, withRoutes } from '@angular/ssr';
 
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
 import { ServerApiBaseUrlInterceptor } from '@core/http/interceptors/server-api-base-url.interceptor';
+import { ServerTranslateLoader } from '@core/i18n/server-translate.loader';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(withRoutes(serverRoutes)),
+    {
+      provide: TranslateLoader,
+      useClass: ServerTranslateLoader
+    },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: ServerApiBaseUrlInterceptor,
