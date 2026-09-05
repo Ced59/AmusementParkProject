252c204b3db4c7f35e7baf44a5d795015cb3274a
Ced59 <c.caudron59@gmail.com>
2026-09-05 20:31:18 +0200

    refactor(passport): isolate query contracts

---
+++ API/AmusementPark.Application/Features/Passport/Queries/GetPassportParkStatisticsQuery.cs
@@ -0,0 +1,10 @@
+using AmusementPark.Application.Abstractions;
+using AmusementPark.Application.Errors;
+using AmusementPark.Application.Features.Passport.Results;
+
+namespace AmusementPark.Application.Features.Passport.Queries;
+
+public sealed record GetPassportParkStatisticsQuery(
+    string UserId,
+    string ParkId)
+    : IQuery<ApplicationResult<PassportParkStatisticsResult>>;
---
+++ API/AmusementPark.Application/Features/Passport/Queries/GetVisitQuery.cs
@@ -0,0 +1,9 @@
+using AmusementPark.Application.Abstractions;
+using AmusementPark.Application.Errors;
+using AmusementPark.Application.Features.Passport.Results;
+
+namespace AmusementPark.Application.Features.Passport.Queries;
+
+public sealed record GetVisitQuery(
+    string UserId,
+    string VisitId) : IQuery<ApplicationResult<VisitResult>>;
